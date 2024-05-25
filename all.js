7fe44ded48f37286ed31eef761fc671912ee14a1
kerorinrin <128007877+kerorinrin@users.noreply.github.com>
2024-05-25 19:44:39 +0800

    0502ver.

--- all.js
+++ all.js
@@ -1,12 +1,27 @@
 $(function(){
 
 $('.btn').click(function(){
-
   $('.menu').toggleClass('close');
   $('.closure').toggleClass('close');
   $('nav ul').toggleClass('close');
+  $('.overlay').toggleClass('close');
 })
 
+$('.overlay').click(function(){
+  $('.menu').toggleClass('close');
+  $('.closure').toggleClass('close');
+  $('nav ul').toggleClass('close');
+  $('.overlay').toggleClass('close');
+})
+
+$('.navHover').hover(function () {
+    // over
+    $(this).addClass('showQoute');
+  }, function () {
+    // out
+    $(this).removeClass('showQoute');
+  }
+);
 
 
 });
