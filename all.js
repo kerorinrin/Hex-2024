f1de9843439e603930de22065f3dd7e0fa3c35be
kerorinrin <128007877+kerorinrin@users.noreply.github.com>
2024-05-29 14:44:24 +0800

    0529ver.
    Adjusting RWD and other effects

--- all.js
+++ all.js
@@ -1,5 +1,6 @@
 $(function(){
 
+  //　 ☆　Mobile Nav menu open&close effect 
 $('.btn').click(function(){
   $('.menu').toggleClass('close');
   $('.closure').toggleClass('close');
@@ -14,6 +15,8 @@ $('.overlay').click(function(){
   $('.overlay').toggleClass('close');
 })
 
+
+//  ☆　header nav a hovering effect
 $('.navHover').hover(function () {
     // over
     $(this).addClass('showQoute');
@@ -21,7 +24,37 @@ $('.navHover').hover(function () {
     // out
     $(this).removeClass('showQoute');
   }
-);
+)
+
+
+//  ☆　footer button hover effect(svg color change)
+$('#footerBtn1').hover(function () {
+  // mouse enter
+  $('#footerSvg1').find('path').attr('fill','#fff');
+}, function () {
+  // out
+  $('#footerSvg1').find('path').attr('fill','#5b5b5b');
+}
+)
+$('#footerBtn2').hover(function () {
+  // mouse enter
+  $('#footerSvg2').find('path').attr('fill','#fff');
+}, function () {
+  // out
+  $('#footerSvg2').find('path').attr('fill','#5b5b5b');
+}
+)
+$('#footerBtn3').hover(function () {
+  // mouse enter
+  $('#footerSvg3').find('path').attr('fill','#fff');
+}, function () {
+  // out
+  $('#footerSvg3').find('path').attr('fill','#5b5b5b');
+}
+)
+
+
+
 
 
 });
